86f77eea54d40d942613e41519b497087bf92e0f
mj <minai621@naver.com>
2024-05-01 12:00:57 +0900

    init assignment2

---
+++ assignment2/CTetris.cpp
@@ -0,0 +1,65 @@
+﻿#include "CTetris.h"
+
+using namespace std;
+
+///**************************************************************
+/// static member variables and functions
+///**************************************************************
+
+Matrix *** CTetris::setOfColorBlockObjects = NULL;
+
+void CTetris::init(int **setOfColorBlockArrays, int nTypes, int nDegrees) {
+  if (setOfColorBlockObjects != NULL) // already allocated?
+    deinit();
+
+  Tetris::init(setOfColorBlockArrays, nTypes, nDegrees); // call superclass' function
+
+  // write the rest of this function!!
+}
+
+void CTetris::deinit(void) {
+  Tetris::deinit(); // call superclass' function
+
+  for (int t = 0; t < numTypes; t++)
+    for (int d = 0; d < numDegrees; d++)
+      delete setOfColorBlockObjects[t][d]; // deallocate matrix objects
+
+  for (int t = 0; t < numTypes; t++)
+    delete [] setOfColorBlockObjects[t]; // deallocate 1d array of Matrix pointers
+
+  delete []  setOfColorBlockObjects; // deallocate 1d array of pointers to Matrix pointer arrays
+
+  setOfColorBlockObjects = NULL;
+}
+
+///**************************************************************
+/// dynamic member variables and functions
+///**************************************************************
+
+/// constructors and destructor
+
+CTetris::CTetris(int cy, int cx) : Tetris(cy, cx) {
+  iCScreen = new Matrix(iScreen);
+  oCScreen = new Matrix(oScreen);
+  currCBlk = NULL;
+}
+
+CTetris::~CTetris() {
+  delete iCScreen;
+  delete oCScreen;
+}
+
+/// mutators
+TetrisState CTetris::accept(char key) {
+
+  TetrisState _state;
+
+  // complete this function!!
+  
+  _state = Tetris::accept(key); // call superclass' function
+
+  // you can use the following code if you want to
+  // oCScreen = deleteFullLines(oCScreen, currCBlk, top, wallDepth);
+
+  return _state;
+}
